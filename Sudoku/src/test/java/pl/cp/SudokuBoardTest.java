37bafcace9568bb0e36870d411e123f6ee406baa
Marek Kopania <m.k.kopania@gmail.com>
2023-03-11 18:24:35 +0100

    Added test for boxes

--- Sudoku/src/test/java/pl/cp/SudokuBoardTest.java
+++ Sudoku/src/test/java/pl/cp/SudokuBoardTest.java
@@ -15,6 +15,7 @@ class SudokuBoardTest extends SudokuBoard {
         SudokuBoard sudoku = new SudokuBoard();
         SudokuBoard sudoku2 = new SudokuBoard();
         sudoku.fillBoard(0, 0);
+        //Test for rows and columns
         for (int i = 0; i < 9; i++) {
             Set<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
             Set<Integer> set2 = new HashSet<>();
@@ -25,6 +26,19 @@ class SudokuBoardTest extends SudokuBoard {
             }
             assertEquals(set, set2);
             assertEquals(set, set3);
+        }
+        //Test for the boxes
+        for (int i = 0; i < 9; i += 3) {
+            Set<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+            Set<Integer> set2 = new HashSet<>();
+            for (int j = i; j < i + 3; j++) {
+                for (int k = 0; k < 9; k += 3) {
+                    for (int l = k; l < k + 3; l++) {
+                        set2.add(sudoku.getNumber(j, l));
+                    }
+                }
+            }
+            assertEquals(set, set2);
         }
     }
 
