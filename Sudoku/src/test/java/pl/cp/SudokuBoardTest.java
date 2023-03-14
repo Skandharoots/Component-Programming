c25e873594c47e032799c7e93e62cec665b7077f
Marek Kopania <m.k.kopania@gmail.com>
2023-03-14 13:05:23 +0100

    Finalized project

--- Sudoku/src/test/java/pl/cp/SudokuBoardTest.java
+++ Sudoku/src/test/java/pl/cp/SudokuBoardTest.java
@@ -30,15 +30,16 @@ class SudokuBoardTest extends SudokuBoard {
             assertEquals(set, set3);
         }
         //Test for the boxes
+        Set<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
         for (int i = 0; i < 9; i += 3) {
-            Set<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-            Set<Integer> set2 = new HashSet<>();
             for (int k = 0; k < 9; k += 3) {
+                Set<Integer> set2 = new HashSet<>();
                 for (int j = i; j < i + 3; j++) {
                     for (int l = k; l < k + 3; l++) {
                         set2.add(sudoku.getNumber(j, l));
                     }
                 }
+                System.out.println(set2);
                 assertEquals(set, set2);
             }
         }
@@ -60,25 +61,8 @@ class SudokuBoardTest extends SudokuBoard {
         sudoku.fillBoard(0, 0);
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-
-        // After this all System.out.println() statements will come to outContent stream.
-
-        // So, you can normally call,
-        sudoku.printSudoku(); // I will assume items is already initialized properly.
-
-        //Now you have to validate the output. Let's say items had 1 element.
-        // With name as FirstElement and number as 1.
-        String notExpectedOutput  = "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n" +
-                "1 2 3 4 5 6 7 8 9\n"; // Notice the \n for new line.
-
-        // Do the actual assertion.
+        sudoku.printSudoku();
+        String notExpectedOutput  = null;
         assertNotEquals(notExpectedOutput, outContent.toString());
     }
 }
