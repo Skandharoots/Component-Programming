1a07f1b7484a137a5b4fc2c2f38fd4696104a43a
FKrylecki <kryleckif@gmail.com>
2023-05-06 20:28:55 +0200

    Dao additional JUnit test

--- Sudoku/src/test/java/pl/cp/SudokuBoardDaoTest.java
+++ Sudoku/src/test/java/pl/cp/SudokuBoardDaoTest.java
@@ -3,7 +3,8 @@ package pl.cp;
 import org.junit.jupiter.api.Test;
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SudokuBoardDaoTest {
     @Test
@@ -15,6 +16,17 @@ public class SudokuBoardDaoTest {
         dao.write(board);
         SudokuBoard board2 = (SudokuBoard) dao.read();
         assertEquals(board, board2);
+    }
 
+    @Test
+    void testExceptionDao() {
+        SudokuBoardDaoFactory factory1 = new SudokuBoardDaoFactory();
+        Dao dao1 = factory1.getFileDao("Sudoku2.txt");
+        SudokuBoardDaoFactory factory2 = new SudokuBoardDaoFactory();
+        Dao dao2 = factory2.getFileDao("SudokuNoFile.txt");
+        DaoExceptions thrown1 = assertThrows(DaoExceptions.class, () -> dao1.read(), "Reader failed");
+        // thrown2 = assertThrows(DaoExceptions.class, () -> dao2.write(new SudokuBoard(null)), "Write failed");
+        assertTrue(thrown1.getMessage().contentEquals("Reader failed"));
+        //assertTrue(thrown2.getMessage().contentEquals("Write failed"));
     }
 }
