d6009af35ce00f0dff53d1d60485dafbc1137250
Marek Kopania <234760@edu.p.lodz.pl>
2023-05-12 17:18:30 +0200

    update: added Model dependencies to ViewProject

--- Sudoku/View/src/main/java/module-info.java
+++ Sudoku/View/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module pl.cp.first.view {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires ModelProject;
 
     opens pl.cp.first to javafx.fxml;
     exports pl.cp.first;
--- Sudoku/View/src/main/java/pl/cp/first/MainController.java
+++ Sudoku/View/src/main/java/pl/cp/first/MainController.java
@@ -2,8 +2,11 @@ package pl.cp.first;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import pl.cp.SudokuBoard;
 
-public class HelloController {
+public class MainController {
+
+    private SudokuBoard board;
     @FXML
     private Label welcomeText;
 
